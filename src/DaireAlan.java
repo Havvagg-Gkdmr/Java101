e3fbfece3f647842c05f05c3b4337f48c340c4f1
Havva Gül Gökdemir <cerengokdemir@hotmail.com>
2024-11-04 13:44:10 +0300

    Java101 Alıştırmaları

--- src/DaireAlan.java
+++ src/DaireAlan.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 public class DaireAlan {
     public static void main(String[] args){
 
-        int r ;
+        int r ,aci;
         double pi =3.14;
-        double a,c;
+        double alan,cevre,dAlan;
 
         Scanner inp=new Scanner(System.in);
         System.out.print("Dairenin yarıçapını giriniz : ");
         r=inp.nextInt();
 
-        a=pi*r*r;
-        System.out.println("Dairenin alanı : "+a);
-        c=2*pi*r;
-        System.out.println("Dairenin çevresi : "+c);
+        alan=pi*r*r;
+        System.out.println("Dairenin alanı : "+alan);
+        cevre=2*pi*r;
+        System.out.println("Dairenin çevresi : "+cevre);
+        System.out.print("Merkez açının ölçüsünü giriniz : ");
+        aci=inp.nextInt();
+        dAlan=(pi*(r*r)*aci)/360;
+        System.out.println("Daire diliminin alanı : "+dAlan);
 
 
 
